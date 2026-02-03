7c0d7f31bb2cbfa6391889f06c29656f38a926ee
Duong Huy Phuoc Chu <104732864+fuocchu@users.noreply.github.com>
2026-02-04 01:39:56 +0800

    Level-0: Implement skeleton chatbot with greeting and exit (fixed readme)

---
+++ src/main/java/TreeBuddy.java
@@ -0,0 +1,7 @@
+public class TreeBuddy {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm TreeBuddy");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
