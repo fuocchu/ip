94bd16c87dcba931393b2594bb800c70ae6b5e1a
Duong Huy Phuoc Chu <104732864+fuocchu@users.noreply.github.com>
2026-02-04 02:00:19 +0800

    Level-1: Echo user input

--- src/main/java/TreeBuddy.java
+++ src/main/java/TreeBuddy.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class TreeBuddy {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // Level 0
         System.out.println("Hello! I'm TreeBuddy");
         System.out.println("What can I do for you?");
+
+        boolean exit = false;
+        while (!exit) {
+            String input = sc.nextLine().trim();
+            if (input.equals("bye")) {
+                exit = true;
+            } else {
+                System.out.println(input); // Level 1
+            }
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
