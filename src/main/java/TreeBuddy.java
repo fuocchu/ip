fe53721404ab24b80d29221d2acbbd66d259a126
Duong Huy Phuoc Chu <104732864+fuocchu@users.noreply.github.com>
2026-02-04 02:10:08 +0800

    Level-3: Mark As Done

--- src/main/java/TreeBuddy.java
+++ src/main/java/TreeBuddy.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class TreeBuddy {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
         System.out.println("Hello! I'm TreeBuddy");
         System.out.println("What can I do for you?");
 
@@ -18,12 +18,39 @@ public class TreeBuddy {
                 if (tasks.isEmpty()) {
                     System.out.println("Your list is empty.");
                 } else {
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < tasks.size(); i++) {
-                        System.out.println((i + 1) + ". " + tasks.get(i));
+                        System.out.println((i + 1) + "." + tasks.get(i));
                     }
                 }
+            } else if (input.startsWith("mark ")) {
+                try {
+                    int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                    if (index >= 0 && index < tasks.size()) {
+                        tasks.get(index).markAsDone();
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println("  " + tasks.get(index));
+                    } else {
+                        System.out.println("Invalid task number.");
+                    }
+                } catch (Exception e) {
+                    System.out.println("Invalid command format. Use: mark <task_number>");
+                }
+            } else if (input.startsWith("unmark ")) {
+                try {
+                    int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                    if (index >= 0 && index < tasks.size()) {
+                        tasks.get(index).unmark();
+                        System.out.println("OK, I've marked this task as not done yet:");
+                        System.out.println("  " + tasks.get(index));
+                    } else {
+                        System.out.println("Invalid task number.");
+                    }
+                } catch (Exception e) {
+                    System.out.println("Invalid command format. Use: unmark <task_number>");
+                }
             } else {
-                tasks.add(input);
+                tasks.add(new Task(input));
                 System.out.println("added: " + input);
             }
         }
